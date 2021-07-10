ad6a6c76ffb5315988a3239e4c5e8a40105a385c
Verina Armanyous <verina.samir99@gmail.com>
2021-07-10 15:11:24 -0600

    Project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Quiz from './Quiz'
+import Quiz from './components/Quiz/Quiz'
 
 export default function App (){
     return ( <Quiz />)
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {questions} from './questions';
+
+export default function Quiz(){
+    const [displayedQuestion, setDisplayedQuestion] = useState(0);
+    const [displayScore, setDisplayScore] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleAnswerButton = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1)
+        }
+        const nextQuestion = displayedQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setDisplayedQuestion(nextQuestion)
+        } else {
+            setDisplayScore(true)
+        }
+};
+    return(
+        <div className='question-card'>
+            {displayScore? (
+                <div className='score-section'>
+                    you scored {score} out of {questions.length}
+                </div>) : (
+            <>
+                <div className='question-section'>
+                    <div className='question-count'>Question {displayedQuestion +1} / {questions.length}</div>
+                    <div className='question-text'>{questions[displayedQuestion].questionText}</div>
+                    <div className='question-answers'>
+                        {questions[displayedQuestion].answerOptions.map((answerOption) => <button onClick={() => handleAnswerButton(answerOption.isCorrect)}>{answerOption.answerText}</button>)}
+                    </div>
+                </div>
+            </>)}
+        </div>
+);
+}
