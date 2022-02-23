a51f9c864d018e7940e5895380d7fb88d233306e
Verina <verina.armanyous@uni.minerva.edu>
2022-02-22 17:59:16 -0800

    Indicate answer user selected and add icons for accessibility

--- src/components/Quiz/Answer.jsx
+++ src/components/Quiz/Answer.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import CircleIcon from '@mui/icons-material/Circle';
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 /**
  * This component shows a set of possible answers that includes one correct answer
  * @param {Array} questions This includes a list of questions where each question 
@@ -16,16 +18,47 @@ import CircleIcon from '@mui/icons-material/Circle';
  }}
  */
 function Answer({questions, indexOfDisplayedQuestion, displayAnswers, handleAnswerButton}) {
+    const [selectedAnswer, setSelectedAnswer] = useState(null)
+    const [iconShown, setIconShown] = useState(null)
+    const getClass = (answerOption) => {
+        if (!displayAnswers){
+            return ["bg-regular answer-button", "none"]
+        }
+        if (answerOption.answerText.toString()===selectedAnswer){
+            if (answerOption.isCorrect){
+                return ['bg-selected-correct answer-button', 'tick']
+                
+            }
+            else{
+                return ['bg-selected-incorrect answer-button', 'cross']
+                
+            }
+        }
+        if (answerOption.isCorrect) {
+            return ["bg-correct answer-button", 'tick']
+        } else{
+            return  ["bg-regular answer-button", 'none']
+        }
+
+    }
+
+    const handleAnswerButtonWrapper = (isCorrect, e) => {
+        setSelectedAnswer(e.target.textContent)
+        handleAnswerButton(isCorrect);
+        
+    }
+
     return (
         <div className='answer-section'>
             <Box>
             {questions[indexOfDisplayedQuestion].answerOptions.map((answerOption) => {
-                const bgColor = displayAnswers ?  answerOption.isCorrect === true? "bg-correct" : "bg-incorrect": "bg-regular";
+                const [stylingClass, iconType] = getClass(answerOption)
+                console.log(iconType)
             return ( 
-            <Button className = {`${bgColor} answer-button`} onClick={() => handleAnswerButton(answerOption.isCorrect)} key={answerOption.key}>
-
+            <li onClick={(e) => {handleAnswerButtonWrapper(answerOption.isCorrect,e)}} key={answerOption.key} className = {stylingClass} >
                 {String(answerOption.answerText)}
-            </Button>
+                {iconType==='none'? "":iconType==='tick'? <DoneIcon></DoneIcon>:<CloseIcon></CloseIcon>}
+            </li>
             )})}
             </Box>
         </div>
