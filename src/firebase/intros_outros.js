de4d6e30e81f0d61d79eb5fe1421beed89dc95ef
Verina <verina.armanyous@uni.minerva.edu>
2022-02-28 18:15:36 -0800

    Update intos and outros content

--- src/firebase/intros_outros.js
+++ src/firebase/intros_outros.js
@@ -1,26 +1,27 @@
 export const information = {"Easy":{
     intro: {
-        text: "This module shows examples of how media coverage, movies, and social media can create and perpetuate cultural stereotypes. Notice how your surrounding environment and things you are exposed to on a daily basis can unconsciously shape your perceptions of the world. ",
+        text: "This module shows how media coverage, movies, and social media can create and perpetuate cultural stereotypes. Notice how your surrounding environment and things you are exposed to on a daily basis can subconsciously shape your perceptions of the world. ",
         img: ""},
     'outro': {
-        text: "As you saw in this module, media, movies, social media, and other information sources you encounter daily can slowly but surely influence your views on the world and in. Be aware of how such information can persist cultural misconceptions in your mind.",
+        text: "As you saw in this module, media, movies, social media, and other information sources you encounter daily can slowly but surely influence your views on the world. Be aware of how such information can persist cultural misconceptions in your mind.",
         img: ""}}, 
     "Medium":{
     'intro': {
         text: "The current most popular world map, a projection developed by Mercator in 1569 shown below, is misleading — it distorts the relative size of some countries and continents to the advantage of the West. In this module, we debunk common misconceptions related to the world map that affect our understanding of the world.",
         img: "https://res.cloudinary.com/dcatrgtx0/image/upload/v1638726709/medium/world-map_zmc12l.png"},
     'outro': {
-        text: "Why does this matter, you may ask? Mercator's map reinforces imperialist views as it exaggerates the size of aged imperialist power and shrinks other countries and continents like Africa and South America, and that in turn shapes the perception of power around the world as humans unconsciously equate size with power and importance. Even the Gall-Peters Projection shown earlier, although it shows the correct sizes of countries, it distorts the shape. No flat map of the world can be perfect as distortion is bound to happen, but it is good to be aware of how they influence our world perceptions. Check out this video from West Wing that discusses the consequences of using Mercator's map.",
+        text: "Why does this matter, you may ask? Mercator's map reinforces imperialist views as it exaggerates the size of aged imperialist power and shrinks other countries and continents like Africa and South America, and that in turn shapes the perception of power around the world as humans equate size with power and importance. Think about how the widespread use of this map at school, work, and everywhere can influence our world perceptions and what we can do to combat this.",
         img: "", 
-        video:"https://www.youtube.com/embed/vVX-PrBRtTY"}
+        video:""}
         },
     "Epic":{
     'intro': {
-        text: "In this module, you will see some statistics about the world that some people might find surprising. Enjoy another perspective of the world using numbers.",
+        text: "This module is a continuation of the world map module. Apply the knowledge you learned so far to see how your perspective of the world has changed through learning about the world map.",
         img: "" },
     'outro': {
-        text: "Great job for making it that far! Think about how you can apply the knowledge you just gained to recognize and resist cultural misconceptions.",
-        img: "https://media.giphy.com/media/RIjVnmAtOD4l4c2Ert/giphy.gif"   }},
+        text: "Great job! You are now an expert at maps. Check out this video from West Wing that summarizes the main points you just learned in the world map module.",
+       img:"",
+        video: "https://www.youtube.com/embed/vVX-PrBRtTY"}},
     "Legendary":{
         'intro': {
             text: "In this module, you will see some statistics about the world that some people might find surprising. Enjoy another perspective of the world using numbers.",
