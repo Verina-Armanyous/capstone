2413deb0651569fa3937ea9fbfd0bfb318fa9b91
Verina <verina.armanyous@uni.minerva.edu>
2022-03-27 15:56:10 -0700

    Create a button to reset game progress

--- src/components/Home/ResetProgressButton.jsx
+++ src/components/Home/ResetProgressButton.jsx
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
 import Button from '@mui/material/Button';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
 
 export default function ResetProgressButton() {
     const clearCacheData = () => {
-        localStorage.removeItem("levels-state");
+        localStorage.clear();
         window.location.reload();
 
         }
   
     return (
       <div>
-        <Button variant="outlined" style={{backgroundColor: "#21b6ae"}} onClick={clearCacheData}>
-          Reset Progress
-        </Button>
+        <AppBar style={{ background: 'transparent', boxShadow: 'none'}}>
+          <Toolbar>
+            <Button variant="outlined" style={{backgroundColor: "#cc3300", color:'white'}} onClick={clearCacheData}>
+              <RestartAltIcon style={{color: "white"}}/>
+              Reset Progress
+            </Button>
+          </Toolbar>
+        </AppBar>
+
       </div>
     );
   }
