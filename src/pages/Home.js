8acc3deb8a51fe86e03d13a847003d0c37f58bd2
Verina <verina.armanyous@uni.minerva.edu>
2022-03-27 15:56:29 -0700

    Add the reset progress button to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import React, {useContext} from 'react';
 import LevelCard from '../components/Home/LevelCard';
 import LevelsContext from '../state/LevelsContext';
+import ResetProgressButton from '../components/Home/ResetProgressButton';
 export default function Home() {
     const {levels} = useContext(LevelsContext);
-    return <div style={{marginLeft:'auto', justifyContent:'right'}}>
-        {
-          levels.map((level, i) => 
-            <LevelCard key={i} {...level} />
-          )
-        }
-        </div>
+    return(
+      <div>
+        <ResetProgressButton/>
+       <div style={{marginLeft:'auto', justifyContent:'right'}}>
+               {
+                 levels.map((level, i) => 
+                   <LevelCard key={i} {...level} />
+                 )
+               }
+       </div>
+   </div>
+    )
+
+
 }
