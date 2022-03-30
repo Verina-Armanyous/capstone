877c53387aa3f298362e23b252e650af50eba0c1
Verina <verina.armanyous@uni.minerva.edu>
2022-03-30 16:57:27 -0700

    Change the app icon

--- src/components/Quiz/Score.jsx
+++ src/components/Quiz/Score.jsx
@@ -48,7 +48,7 @@ function Score({numberOfCorrectAnswers, totalNumberOfQuestions, level}) {
             <Typography variant="h5">Yay, you scored {numberOfCorrectAnswers} out of {totalNumberOfQuestions}!</Typography>
             <Typography style= {{textAlign:'left'}} gutterBottom variant="h6" component="div">
                         {information[level].outro.text} 
-                    </Typography><Confetti tweenDuration="10"/>
+                    </Typography><Confetti numberOfPieces="100" recycle='false'/>
                     <>
                     {information[level].outro.img !==''? <CardMedia style ={{maxWidth:'95%', padding:'10px', objectFit: 'contain'}}component="img" alt="green iguana" height="400" image={information[level].outro.img }/>:<p></p>}
                     {information[level].outro.video? <iframe width="700" height="550" src={`${information[level].outro.video}?hl=en&amp;cc_load_policy=1;&cc_lang_pref=en`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"></iframe> :<p></p>} </>
